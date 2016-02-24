4f90c5ab4d0f33b546b727992bcdc73c7d9ccc9d
Matteo Kloiber <Matt3o12@Matteos-MacBook-Pro.local>
2016-02-24 13:37:58 +0100

    Added script

---
+++ main.py
@@ -0,0 +1,55 @@
+import requests
+
+URL_UPTIME_ROBOT_IPS = "https://uptimerobot.com/inc/files/ips/IPv4andIPv6.txt"
+URL_CLOUDFLARE_ADD_IP = "https://api.cloudflare.com/client/" \
+        "v4/user/firewall/access_rules/rules"
+
+
+class APIError(Exception):
+    pass
+
+
+def load_ips():
+    r = requests.get(URL_UPTIME_ROBOT_IPS)
+    ips = []
+    for ip in r.text.split("\n"):
+        if ip.strip():
+            ips.append(ip.strip())
+
+    return ips
+
+def add_ip_to_cloudflare(ip, email, secret):
+    headers = {
+        "Content-Type": "application/json", 
+        "X-Auth-Email": email,
+        "X-Auth-Key": secret,
+    }
+    data = {
+        "mode": "whitelist",
+        "configuration": {
+            "target": "ip",
+            "value": ip,
+        },
+        "notes": "uptime_robot_ip",
+    }
+    r = requests.post(URL_CLOUDFLARE_ADD_IP, json=data, headers=headers)
+    if not (r.status_code == 200 and r.json().get("success", False)):
+        msg = "API returned error: {}"
+        raise APIError(msg.format(r.json().get("errors", [])))
+
+def ask_for_auth():
+    try:
+        input = raw_input
+    except NameError:
+        pass
+
+    email = input("Email: ").strip()
+    secret = input("API-Secret: ").strip()
+
+    return email, secret
+
+if __name__ == "__main__":
+    auth = ask_for_auth()
+    for ip in load_ips():
+        add_ip_to_cloudflare(ip, *auth)
+
